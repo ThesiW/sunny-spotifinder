6bf9303a8ee5e90d9710461e8209a4e8050fbd7d
samir silwal <samirsilwal316@gmail.com>
2021-06-01 14:11:14 +0200

    still need to work in controller

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,12 +1,15 @@
 class SpotsController < ApplicationController
 
   def index
+    @spot = Spot.all
   end
 
   def show
+   @spot = Spot.find(params[:id])
   end
 
   def randomise
+    @spot ||= Spot.all.shuffle
+    @spot.pop
   end
-
 end
