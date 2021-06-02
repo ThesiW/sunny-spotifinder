e42a16cbfb6ebcf707f7672f414558bc422da1ee
Vincent Li <vincentli002@gmail.com>
2021-06-02 14:21:25 +0200

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_01_094013) do
-
 ActiveRecord::Schema.define(version: 2021_06_01_143900) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
