4fe8dbc5d847721d77acfd5f3341dbca62a78dd7
samir silwal <samirsilwal316@gmail.com>
2021-06-07 09:19:03 +0200

    Done with review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def show
+  @user = User.find(params[:id])
+  @user = Useer.all
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :favourites
   has_many :visits
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one_attached :picture
+
+
+  def user_picture
+    if self.picture.attached?
+        self.picture.key
+    else
+      '04s5p8amefgigaoiipqup8r3f7vo'
+    end
+  end
 end
